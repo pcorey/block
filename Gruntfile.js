a7e6f3c48c15623bba8ba742ebeb59d91f19edb0
Pete Corey <petecorey@gmail.com>
2014-05-29 20:01:30 -0700

    Basic block!
    Images snagged from http://apply.redbullmusicacademy.com/

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,47 @@
+// module.exports = function(grunt) {
+//     pkg: grunt.file.readJSON('package.json'),
+//     grunt.initConfig({
+//     // running `grunt less` will compile once
+//     less: {
+//         development: {
+//             options: {
+//                 paths: ["less"]
+//             },
+//             files: {
+//                 "css/main.css": "less/main.less"
+//             }
+//         }
+//     },
+
+//     // running `grunt watch` will watch for changes
+//     watch: {
+//         less: {
+//             files: ["less/*.less"],
+//             tasks: ["less"]
+//         }
+//     }
+// });
+
+// grunt.loadNpmTasks('grunt-contrib-less');
+// grunt.loadNpmTasks('grunt-contrib-watch');
+
+// });
+
+
+
 module.exports = function(grunt) {
-    grunt.initConfig({
-    // running `grunt less` will compile once
-    less: {
-        development: {
-            options: {
-                paths: ["less"]
-            },
-            files: {
-                "css/main.css": "less/main.less"
-            }
-        }
-    },
 
-    // running `grunt watch` will watch for changes
-    watch: {
-        less: {
-            files: ["less/*.less"],
-            tasks: ["less"]
-        }
-    }
-});
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+  });
 
-grunt.loadNpmTasks('grunt-contrib-less');
-grunt.loadNpmTasks('grunt-contrib-watch');
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  //grunt.registerTask('default', ['less']);
+
+};
